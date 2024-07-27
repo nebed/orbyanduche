8da25a7edcc41968d81b2a9538be291b09153625
Uche Nebed <nebeduch@gmail.com>
2024-07-27 21:04:49 +0300

    update certificate and nginx command

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,11 @@ sudo docker run -d --name $DOCKER_CONTAINER_NAME -p 5000:5000 -v /opt/qrcodes:/a
 sudo mkdir -p $FRONTEND_DIR
 sudo cp -r /opt/orbyanduche/dist/* $FRONTEND_DIR
 
+# Generate nginx certificate
+sudo service nginx stop
+sudo certbot certonly  --standalone -d orbyanduche.nebed.io  --non-interactive --agree-tos -m webmaster@nebed.io
+sudo service nginx start
+
 # Set up Nginx
 sudo cp /opt/orbyanduche/orbyanduche_nginx.conf /etc/nginx/sites-available/$DOMAIN
 sudo ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/
